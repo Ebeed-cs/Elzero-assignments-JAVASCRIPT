614050b49cb8bb8680ca90d156f58a8c9408efe9
Ahmed Ebeed <aebeedcs@gmail.com>
2024-10-04 22:36:50 +0300

    ADD TASK [1 - 9]

---
+++ TASK [1 - 9]/task-six.js
@@ -0,0 +1,10 @@
+    // first one using single line comment
+    // second one using multible line comment
+
+// console.log("Iam In Console");
+// document.write("Iam In Page");
+
+/*
+console.log("Iam In Console");
+document.write("Iam In Page");
+*/
